36ad6e6a2f087fc41887af874cd8bfe63c2b706f
s21010 <s21010@std.it-college.ac.jp>
2023-02-21 17:12:08 +0900

    修正

--- app/Controllers/Http/SamplesController.ts
+++ app/Controllers/Http/SamplesController.ts
@@ -2,6 +2,8 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class SamplesController {
   public async index(ctx: HttpContextContract {
+    const msg = 'ID:' + ctx.request.param('id')
+      + ', pass:' + ctx.request.param('pass')
     const data = {
       title: 'Sample',
       message: 'これは、SamplesController'
